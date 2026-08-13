6f06c9fda84342dde83607e7ea08e44bd86fdaf9
shivam8055 <68191231+shivam8055@users.noreply.github.com>
2026-08-13 22:58:56 +0530

    feat(inventory): define read-only inventory repository contract

--- lib/features/inventory/repositories/inventory_repository.dart
+++ lib/features/inventory/repositories/inventory_repository.dart
@@ -3,15 +3,5 @@ import '../models/inventory_item.dart';
 abstract class InventoryRepository {
   Future<List<InventoryItem>> getInventory();
 
-  Future<InventoryItem> createInventoryItem(
-      InventoryItem item,
-      );
-
-  Future<void> updateInventoryItem(
-      InventoryItem item,
-      );
-
-  Future<void> deleteInventoryItem(
-      int id,
-      );
+  Future<List<InventoryItem>> getLowStock();
 }
