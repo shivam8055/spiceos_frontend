44040ce1d9c282b629bd455a7f79605e689755ff
shivam8055 <68191231+shivam8055@users.noreply.github.com>
2026-08-16 19:18:36 +0530

    fix(menu): support list response envelopes

--- lib/features/menu/repositories/menu_repository.dart
+++ lib/features/menu/repositories/menu_repository.dart
@@ -3,13 +3,17 @@ import '../models/menu_item.dart';
 
 class MenuRepository {
   MenuRepository(this._api);
-
   final ApiClient _api;
 
   Future<List<MenuItem>> list({required String restaurantId, required String branchId}) async {
     final response = await _api.get('/admin/menu-items?restaurant_id=$restaurantId&branch_id=$branchId');
     final data = response.data;
-    if (data is! List) return const [];
-    return data.map((item) => MenuItem.fromJson(Map<String, dynamic>.from(item as Map))).toList();
+    if (data is List) {
+      return data.map((item) => MenuItem.fromJson(Map<String, dynamic>.from(item as Map))).toList();
+    }
+    if (data is Map && data['items'] is List) {
+      return (data['items'] as List).map((item) => MenuItem.fromJson(Map<String, dynamic>.from(item as Map))).toList();
+    }
+    return const [];
   }
 }
