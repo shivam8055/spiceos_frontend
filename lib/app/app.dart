484c445d26b3e8e490218e6cd0a592f761ccdf31
shivam8055 <68191231+shivam8055@users.noreply.github.com>
2026-08-18 16:30:24 +0530

    fix(web): add router dependency import

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:spicebox/core/theme/app_theme.dart';
 
 import '../features/auth/providers/auth_notifier.dart';
