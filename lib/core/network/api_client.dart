575ac1d99427294e5fd01f6626c192d94ed6e543
shivam8055 <68191231+shivam8055@users.noreply.github.com>
2026-08-28 00:23:24 +0530

    Support query parameters in API client

--- lib/core/network/api_client.dart
+++ lib/core/network/api_client.dart
@@ -32,56 +32,31 @@ class ApiClient {
             }
             handler.next(options);
           } catch (error) {
-            handler.reject(
-              DioException(
-                requestOptions: options,
-                error: error,
-              ),
-            );
+            handler.reject(DioException(requestOptions: options, error: error));
           }
         },
       ),
     );
-
-    _dio.interceptors.add(
-      LogInterceptor(
-        requestBody: true,
-        responseBody: true,
-      ),
-    );
+    _dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
   }
 
   final Dio _dio;
   final FirebaseAuth _firebaseAuth;
 
-  Future<Response<dynamic>> get(String path) => _dio.get(path);
+  Future<Response<dynamic>> get(String path, {Map<String, dynamic>? queryParameters}) =>
+      _dio.get(path, queryParameters: queryParameters);
 
-  Future<Response<dynamic>> post(String path, [Object? body]) =>
-      _dio.post(path, data: body);
+  Future<Response<dynamic>> post(String path, [Object? body]) => _dio.post(path, data: body);
 
-  Future<Response<dynamic>> postWithHeaders(
-    String path,
-    Object? body, {
-    Map<String, dynamic>? headers,
-  }) =>
-      _dio.post(
-        path,
-        data: body,
-        options: Options(headers: headers),
-      );
+  Future<Response<dynamic>> postWithHeaders(String path, Object? body, {Map<String, dynamic>? headers}) =>
+      _dio.post(path, data: body, options: Options(headers: headers));
 
   Future<Response<dynamic>> postMultipart(String path, FormData body) =>
       _dio.post(path, data: body, options: Options(contentType: 'multipart/form-data'));
 
   Future<Response<dynamic>> patch(String path, Object? body) => _dio.patch(path, data: body);
-
   Future<Response<dynamic>> put(String path, Object? body) => _dio.put(path, data: body);
-
   Future<Response<dynamic>> delete(String path) => _dio.delete(path);
 }
 
-final apiClientProvider = Provider<ApiClient>(
-  (ref) => ApiClient(
-    firebaseAuth: FirebaseAuth.instance,
-  ),
-);
+final apiClientProvider = Provider<ApiClient>((ref) => ApiClient(firebaseAuth: FirebaseAuth.instance));
